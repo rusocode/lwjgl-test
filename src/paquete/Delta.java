cd9c465b01c542bfe3d1fc6ef834636d313df21a
Juan Debenedetti <juandebenedetti94@gmail.com>
2021-01-26 12:34:31 -0300

    Update

--- src/paquete/Delta.java
+++ src/paquete/Delta.java
@@ -7,6 +7,8 @@ import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glMatrixMode;
 import static org.lwjgl.opengl.GL11.glOrtho;
 
+import static org.lwjgl.opengl.GL11.*;
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
 import org.lwjgl.opengl.Display;
@@ -19,12 +21,15 @@ public class Delta {
 	 * 
 	 * La variable delta representa el tiempo transcurrido desde la ultima actualizacion del cuadro. Cuanto mayor sea el
 	 * delta, menor sera la velocidad de fotogramas. Cuanto menor sea el delta, mayor sera la velocidad de fotogramas. Si la
-	 * velocidad de fotogramas esta limitada a 60 fotogramas por segundo, nunca deberia existir un valor delta inferior a
+	 * velocidad de fotogramas esta limitada a 60 fotogramas por segundo, NUNCA deberia existir un valor delta inferior a
 	 * 16. */
 
 	protected boolean running = false;
 
 	private static long lastFrame;
+
+	int x = 10, y = 10;
+	int dx = 1, dy = 1; // Representa el movimiento dinamico
 
 	// Tiempo en milisegundos
 	private static long getTime() {
@@ -66,10 +71,22 @@ public class Delta {
 
 			render();
 
-			System.out.println(getDelta());
+			int delta = (int) getDelta();
+
+			System.out.println(delta);
+
+			x += delta * dx * 0.1; // Velocidad horizontal relentizada a 10 veces (0.1)
+			y += delta * dy * 0.1; // Velocidad vertical
+
+			// Dibuja una caja en las coordenadas xy del primer vertice y en xy del segundo vertice de la esquina
+			glRecti(x, y, x + 30, y + 30); // Esto es exactamente igual al modo inmediato
+
+			// System.out.println(getDelta());
 
 			Display.update();
-			Display.sync(60);
+			/* Lo importante es que Delta siempre sera mayor o igual a 16, por que lo unico que le puede pasar a nuestra velocidad
+			 * de fotogramas es que disminuira. */
+			Display.sync(60); // Aunque se cambie la cantidad de fotogramas la velocidad del rectangulo siempre sera la misma
 
 		}
 
