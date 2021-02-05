a7257a1b4153371c3668b629c1f6370e82b7cd18
Juan Debenedetti <juandebenedetti94@gmail.com>
2021-02-05 12:45:26 -0300

    Update

---
+++ src/_3D/Main.java
@@ -0,0 +1,15 @@
+package _3D;
+
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+
+		System.out.println(random.nextFloat() * 10f);
+		// System.out.println((random.nextFloat() - 0.5f) * 100f);
+
+	}
+
+}
