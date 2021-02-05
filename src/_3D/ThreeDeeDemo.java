f0fedfa84de9c80286b27edfb540ad6265699d17
Juan Debenedetti <juandebenedetti94@gmail.com>
2021-02-05 00:44:06 -0300

    Update

--- src/_3D/ThreeDeeDemo.java
+++ src/_3D/ThreeDeeDemo.java
@@ -6,6 +6,10 @@ import org.lwjgl.opengl.DisplayMode;
 
 import static org.lwjgl.opengl.GL11.*;
 
+import static org.lwjgl.util.glu.GLU.gluPerspective;
+
+import java.util.Random;
+
 public class ThreeDeeDemo {
 
 	private final int width = 640;
@@ -26,6 +30,11 @@ public class ThreeDeeDemo {
 		setUpDisplay();
 		setUpOpenGL();
 
+		
+		// Puntos aleatorios
+		Point[] points = new Point[1000];
+		Random random = new Random();
+
 		while (!Display.isCloseRequested()) {
 
 			update();
@@ -43,7 +52,7 @@ public class ThreeDeeDemo {
 
 	private void setUpDisplay() {
 		try {
-			Display.setTitle("OpenGL");
+			Display.setTitle("3D");
 			Display.setDisplayMode(new DisplayMode(width, height));
 			Display.create();
 		} catch (LWJGLException e) {
@@ -54,10 +63,21 @@ public class ThreeDeeDemo {
 	}
 
 	private void setUpOpenGL() {
+
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
-		glOrtho(0, 640, 0, 480, 1, -1);
+
+		/* Cree una nueva perspectiva con un angulo de 30 grados (campo de vision), relacion de aspecto de 640/480, 0.001f zNear
+		 * (cerca), 100 zFar (lejos). */
+		// +x esta a la derecha
+		// +y esta en la cima
+		// +z es para la camara
+		gluPerspective((float) 30, 640f / 480f, 0.001f, 100);
+
 		glMatrixMode(GL_MODELVIEW);
+
+		// Asegurarse de que los puntos mas cercanos a la camara se muestren delante de los puntos mas alejados
+		glEnable(GL_DEPTH_TEST);
 	}
 
 	private void update() {
@@ -73,6 +93,19 @@ public class ThreeDeeDemo {
 
 	private void input() {
 
+	}
+
+	private static class Point {
+
+		final float x;
+		final float y;
+		final float z;
+
+		public Point(float x, float y, float z) {
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
 	}
 
 }
