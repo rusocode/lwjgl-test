1d5770deabae0781491d0fc445afc2a63db0ee67
Juan Debenedetti <juandebenedetti94@gmail.com>
2021-03-10 19:31:47 -0300

    Fixed y cambios

--- src/ejemplos/Input.java
+++ src/ejemplos/Input.java
@@ -16,10 +16,13 @@ import static org.lwjgl.opengl.GL11.*;
 // Uso de las clases de teclado y mouse con LWJGL
 public class Input {
 
-	private static final List<Box> shapes = new ArrayList<Box>(16);
-	private static boolean somethingIsSelected = false;
+	// Crea una lista constante de clase (estatica) para agregar formas (shapes) de tipo Box
+	private static final List<Box> shapes = new ArrayList<Box>(5);
 
-	protected boolean running = false;
+	private static final int BOX_HEIGHT = 50;
+	private static final int BOX_WIDHT = 50;
+
+	private static boolean somethingIsSelected = false; // ?
 
 	public static void main(String args[]) {
 		try {
@@ -42,10 +45,8 @@ public class Input {
 
 		create();
 
-		running = true;
-
 		// Bucle de renderizacion en donde se realiza el manejo de entradas, la logica del juego y la adm de recursos
-		while (running && !Display.isCloseRequested()) {
+		while (!Display.isCloseRequested()) {
 
 			render();
 
@@ -65,8 +66,10 @@ public class Input {
 		// Creacion del lente para la transformacion de la escena 3D a 2D en la pantalla
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
-		/* La razon por la que tienes que invertir getDY() es porque OpenGL esta destinado a tener el origen en la parte
-		 * superior izquierda. */
+		/*
+		 * La razon por la que tienes que invertir getDY() es porque OpenGL esta destinado a tener el origen en la parte
+		 * superior izquierda.
+		 */
 		// Creacion del sistema de coordenadas de vertice 2D
 		// Esquina superior izquierda: glOrtho(0, 640, 480, 0, 1, -1)
 		// Esquina inferior izquierda: glOrtho(0, 640, 0, 480, 1, -1)
@@ -84,17 +87,20 @@ public class Input {
 		// Posicion
 		// System.out.println("X=" + Mouse.getX() + ":Y=" + Mouse.getY());
 
-		/* Keyboard.next() obtiene el proximo evento de teclado. Puede consultar que clave causo el evento utilizando
-		 * getEventKey. Para obtener el estado de esa clave, para ese evento, use getEventKeyState; finalmente use
-		 * getEventCharacter para obtener el caracter de ese evento. */
+		/* Keyboard.next() obtiene el proximo evento del teclado, capturando una sola pulsacion de tecla. */
 		while (Keyboard.next()) {
 
-			// Si se pulso la tecla C y si la tecla estaba presionada getEventKeyState() o falso si se solto, entonces...
-			if (Keyboard.isKeyDown(Keyboard.KEY_C) && Keyboard.getEventKeyState()) shapes.add(new Box(10, 10));
-
-			// Keyboard.getEventKey() == Keyboard.KEY_C <-- Otra forma de comprobar si se pulso una tecla
+			// Elimina todas las cajas
+			if (Keyboard.isKeyDown(Keyboard.KEY_L)) shapes.clear();
 
 		}
+
+		/*
+		 * Si se pulso la tecla C, entonces...
+		 * Resto el ancho de la caja al limite x para no pasarse de este.
+		 */
+		if (Keyboard.isKeyDown(Keyboard.KEY_C))
+			shapes.add(new Box((int) (Math.random() * (640 - BOX_WIDHT)), (int) (Math.random() * (480 - BOX_HEIGHT))));
 
 		// Si se pulso la tecla escape, cierra la aplicacion
 		if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
@@ -105,20 +111,20 @@ public class Input {
 		// getX() devuelve la posicion absoluta del eje x
 		// getDX() devuelve el movimiento en el eje x desde la ultima vez que se llamo a getDX()
 
-		// Controla cada caja con el for
+		// Controla cada caja con un for each
 		for (final Box box : shapes) {
 
 			// Cuando se selecciona la caja
 			// "480 - Mouse.getY()" se invierte Y para el origen superior izquierdo
-			if (Mouse.isButtonDown(0) && box.isInBounds(Mouse.getX(), Mouse.getY()) && !somethingIsSelected) {
-				somethingIsSelected = true;
+			if (Mouse.isButtonDown(0) && box.isInBounds(Mouse.getX(), Mouse.getY()) /* && !somethingIsSelected */) {
+				// somethingIsSelected = true;
 				box.selected = true;
 			}
 
 			// Cuando se suelta la caja
 			if (Mouse.isButtonDown(1)) {
 				box.selected = false;
-				somethingIsSelected = false;
+				// somethingIsSelected = false;
 			}
 
 			if (box.selected) box.update(Mouse.getDX(), Mouse.getDY()); // "-Mouse.getDY()" se invierte Y para el origen superior izquierdo
@@ -146,7 +152,7 @@ public class Input {
 
 		// Si se selecciono la caja dentro sus limites
 		boolean isInBounds(int mouseX, int mouseY) {
-			return mouseX > x && mouseX < x + 50 && mouseY > y && mouseY < y + 50;
+			return mouseX >= x && mouseX <= x + 50 && mouseY >= y && mouseY <= y + 50;
 		}
 
 		// dx/dy significan delta-x y delta-y
@@ -163,9 +169,9 @@ public class Input {
 			glBegin(GL_QUADS);
 			// Conjuntos de vertices (puntos) que juntos forman una primitiva (triangulos), dando un cuadrado como resultado
 			glVertex2i(x, y); // Posicion del primer vertice x=0;y=0
-			glVertex2i(x + 50, y); // Posicion del segundo vertice x=x+50;y=0
-			glVertex2i(x + 50, y + 50); // Posicion del tercer vertice x=x+50;y=y+50
-			glVertex2i(x, y + 50); // Posicion del cuarto vertice x=0;y=y+50
+			glVertex2i(x + BOX_WIDHT, y); // Posicion del segundo vertice x=x+50;y=0
+			glVertex2i(x + BOX_WIDHT, y + BOX_HEIGHT); // Posicion del tercer vertice x=x+50;y=y+50
+			glVertex2i(x, y + BOX_HEIGHT); // Posicion del cuarto vertice x=0;y=y+50
 			glEnd();
 
 		}
