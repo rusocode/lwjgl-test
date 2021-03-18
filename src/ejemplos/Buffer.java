77cb255cf6d9ad50a06419d9981d29657f0d86fb
Juan Debenedetti <juandebenedetti94@gmail.com>
2021-03-18 10:01:50 -0300

    Update

--- src/ejemplos/Buffer.java
+++ src/ejemplos/Buffer.java
@@ -18,10 +18,12 @@ public class Buffer {
 
 	public static void main(String[] args) {
 
-		/* Un buffer es simplemente un bloque de memoria que contiene algunos datos. Para nuestros propositos, puede
+		/*
+		 * Un buffer es simplemente un bloque de memoria que contiene algunos datos. Para nuestros propositos, puede
 		 * considerarlo como una serie de elementos. Sin embargo, en lugar de acceso aleatorio (por ejemplo, matriz[i]), los
 		 * buffers leen y escriben datos relativos a su posicion actual. Para demostrarlo, digamos que deseamos crear un buffer
-		 * que contenga cuatro bytes y luego leer esos bytes. Lo crearias asi: */
+		 * que contenga cuatro bytes y luego leer esos bytes. Lo crearias asi:
+		 */
 
 		ByteBuffer buffer = BufferUtils.createByteBuffer(4); // LWJGL incluye utilidades para crear facilmente buffers
 
@@ -31,8 +33,10 @@ public class Buffer {
 		buffer.put((byte) 45);
 		buffer.put((byte) 127);
 
-		/* Una vez que necesite leer los datos, debe cambiar el buffer del modo escritura al modo lectura usando el metodo
-		 * flip(). */
+		/*
+		 * Una vez que necesite leer los datos, debe cambiar el buffer del modo escritura al modo lectura usando el metodo
+		 * flip().
+		 */
 		buffer.flip(); // Restablece la posicion a cero
 
 		// Itera cada posicion del buffer
@@ -65,9 +69,11 @@ public class Buffer {
 
 		/* . . . */
 
-		/* Entonces, ¿Como se relaciona esto con LWJGL y OpenGL? Hay dos formas comunes en las que usara buffers: escribiendo
+		/*
+		 * Entonces, ¿Como se relaciona esto con LWJGL y OpenGL? Hay dos formas comunes en las que usara buffers: escribiendo
 		 * datos en GL (es decir, cargando datos de textura en la GPU) o leyendo datos de GL (es decir, leyendo datos de textura
-		 * de la GPU u obteniendo un cierto valor del controlador). */
+		 * de la GPU u obteniendo un cierto valor del controlador).
+		 */
 
 		// Digamos que estamos creando una textura RGBA rojo 1x1, nuestra configuracion del buffer se veria asi:
 		int width = 1; // 1 pixel de ancho
@@ -86,16 +92,20 @@ public class Buffer {
 		// Ejemplo de como obtener datos de GL
 		IntBuffer intBuffer = BufferUtils.createIntBuffer(16); // Crea un buffer con 16 elementos
 
-		/* Esto llamara al meotodo put() relativo con el tamaño maximo de texturas, luego continue "poniendo" ceros hasta que se
+		/*
+		 * Esto llamara al meotodo put() relativo con el tamaño maximo de texturas, luego continue "poniendo" ceros hasta que se
 		 * alcance la capacidad del buffer, entonces volteara nuestro buffer.
 		 * 
 		 * Como se describe en los documentos, GL_MAX_TEXTURE_SIZE nos dara un valor, pero como glGetInteger puede devolver
 		 * hasta 16 elementos, LWJGL espera que nuestro buffer tenga al menos eso como capacidad. Siempre que sea posible, debe
-		 * intentar reutilizar los buffers en lugar de crear siempre nuevos. */
+		 * intentar reutilizar los buffers en lugar de crear siempre nuevos.
+		 */
 		// glGetInteger(GL_MAX_TEXTURE_SIZE);
 
-		/* Dado que nuestro buffer ya esta volteado, nuestra posicion sera cero... para que podamos seguir adelante y tomar el
-		 * primer elemento. */
+		/*
+		 * Dado que nuestro buffer ya esta volteado, nuestra posicion sera cero... para que podamos seguir adelante y tomar el
+		 * primer elemento.
+		 */
 		int maxSize = glGetInteger(GL_MAX_TEXTURE_SIZE); // intBuffer.get();
 
 	}
