64b654a7570a7866758e9c68e97a5887c49ca41b
Juan Debenedetti <juandebenedetti94@gmail.com>
2021-03-18 09:55:22 -0300

    Update

--- src/renderizados/AdvancedRendering.java
+++ src/renderizados/AdvancedRendering.java
@@ -14,10 +14,14 @@ import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
 import static org.lwjgl.util.glu.GLU.gluPerspective;
 
+/**
+ * Muestra una simulacion de particulas similar al espacio con tecnicas de renderizado avanzadas.
+ * 
+ */
 public class AdvancedRendering {
 
-	private final int width = 640;
-	private final int height = 480;
+	private static final int WIDTH = 640;
+	private static final int HEIGHT = 480;
 
 	/** Todos los diferentes modos de render. */
 	private static enum RenderMode {
@@ -25,7 +29,7 @@ public class AdvancedRendering {
 	}
 
 	// Modo Inmediato por defecto
-	private RenderMode mode = RenderMode.IMMEDIATE;
+	RenderMode mode = RenderMode.IMMEDIATE;
 
 	// Velocidad a la que viaja la "camara"
 	private static float speed;
@@ -57,6 +61,51 @@ public class AdvancedRendering {
 		setUpOpenGL();
 		setUpEntities();
 
+		/**
+		 * Display lists
+		 */
+
+		// Genera un ID para la lista de visualizacion
+		displayList = glGenLists(1);
+
+		// Crea la lista de visualizacion usando el ID displayList
+		// Todas las llamadas posteriores para renderizado se almacenaran en la lista de visualizacion
+		glNewList(displayList, GL_COMPILE);
+		glBegin(GL_POINTS);
+
+		for (Point p : points)
+			glVertex3f(p.x, p.y, p.z);
+
+		glEnd();
+
+		// Deja de almacenar llamadas en la lista de visualizacion y compila
+		glEndList();
+
+		/**
+		 * Vertex Arrays and Vertex Buffer Objects
+		 */
+
+		/*
+		 * Crea un nuevo FloatBuffer (arreglo complejo de flotantes) con la capacidad de la longitud de los puntos * 3 (porque
+		 * tenemos 3 vertices por punto).
+		 */
+		vertexArray = BufferUtils.createFloatBuffer(points.length * 3);
+		// Itera los puntos y los guarda en el FloatBuffer
+		for (Point p : points)
+			vertexArray.put(new float[] { p.x, p.y, p.z });
+
+		// Hace que el buffer sea legible para OpenGL
+		vertexArray.flip();
+
+		// Crear el identificador para el VBO
+		vertexBufferObject = glGenBuffers();
+		// Vincular el VBO para su uso (en este caso: almacenar informacion)
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+		// Almacena todo el contenido del FloatBuffer en el VBO
+		glBufferData(GL_ARRAY_BUFFER, vertexArray, GL_STATIC_DRAW);
+		// Desenlaza el VBO
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 		lastFrame = getTime();
 
 		System.out.println("Modo de renderizado configurado a Immediate.");
@@ -67,8 +116,7 @@ public class AdvancedRendering {
 			input();
 
 			Display.update();
-			Display.sync(60);
-
+			Display.sync(30);
 		}
 
 		// Elimina la lista de visualizacion
@@ -77,13 +125,13 @@ public class AdvancedRendering {
 		glDeleteBuffers(vertexBufferObject);
 
 		Display.destroy();
-
+		System.exit(0);
 	}
 
 	private void setUpDisplay() {
 		try {
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
 			Display.setTitle("Renderizados");
-			Display.setDisplayMode(new DisplayMode(width, height));
 			Display.setVSyncEnabled(true);
 			Display.create();
 		} catch (LWJGLException e) {
@@ -123,7 +171,7 @@ public class AdvancedRendering {
 
 	private void render() {
 
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		/*
 		 * Divide el delta entre 16 para obtener un delta de 16 a 60 fps. Si es 60 fps, el delta sera 1.0f y todas las
@@ -137,46 +185,41 @@ public class AdvancedRendering {
 		switch (mode) {
 
 		case DISPLAY_LISTS:
-
 			// Dibuja nuestra lista de visualizacion
 			glCallList(displayList);
 
 			break;
 
 		case VERTEX_BUFFER_OBJECT:
-
 			// Habilita las matrices de vertices (VBO)
 			glEnableClientState(GL_VERTEX_ARRAY);
-
 			// Le dice a OpenGL que use nuestra VBO
 			glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
-
-			// Dile a OpenGL que busque los datos en el VBO vinculado con 3 componentes (xyz) y con el tipo float
+			// Le dice a OpenGL que busque los datos en el VBO vinculado con 3 componentes (xyz) y con el tipo float
 			glVertexPointer(3, GL_FLOAT, 0, 0L);
-
-			// Dile a OpenGL que dibuje los datos proporcionados por el metodo de puntero como puntos. Longitud cantidad de puntos.
+			/*
+			 * Le dice a OpenGL que dibuje los datos proporcionados por el metodo de puntero como puntos. Longitud cantidad de
+			 * puntos.
+			 */
 			glDrawArrays(GL_POINTS, 0, points.length);
-
-			// Desatar la VBO
+			// Desenlaza el VBO
 			glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-			// Deshabilitar matrices de vertices
+			// Deshabilita las matrices de vertices
 			glDisableClientState(GL_VERTEX_ARRAY);
 
 			break;
 
 		case VERTEX_ARRAY_OBJECT:
-
 			// Habilita las matrices de vertices
 			glEnableClientState(GL_VERTEX_ARRAY);
-
-			// Dile a OpenGL que busque los datos en el búfer vertexArray con 3 componentes (xyz)
+			// Le dice a OpenGL que busque los datos en el buffer vertexArray con 3 componentes (xyz)
 			glVertexPointer(3, 0, vertexArray);
-
-			// Dile a OpenGL que dibuje los datos proporcionados por el metodo de puntero como puntos. Longitud cantidad de puntos.
+			/*
+			 * Le dice a OpenGL que dibuje los datos proporcionados por el metodo de puntero como puntos. Longitud cantidad de
+			 * puntos.
+			 */
 			glDrawArrays(GL_POINTS, 0, points.length);
-
-			// Deshabilitar matrices de vertices
+			// Deshabilita las matrices de vertices
 			glDisableClientState(GL_VERTEX_ARRAY);
 
 			break;
@@ -198,13 +241,12 @@ public class AdvancedRendering {
 	private void input() {
 
 		if (Keyboard.isKeyDown(Keyboard.KEY_UP)) speed += 0.01f * delta;
-
 		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) speed -= 0.01f * delta;
 
 		while (Keyboard.next()) {
-
-			if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) speed = 0f;
-
+			if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+				speed = 0f;
+			}
 			if (Keyboard.isKeyDown(Keyboard.KEY_1)) {
 				System.out.println("Modo de renderizado configurado a VBO.");
 				mode = RenderMode.VERTEX_BUFFER_OBJECT;
@@ -232,53 +274,15 @@ public class AdvancedRendering {
 		}
 	}
 
-	private int displayList() {
+	public static void main(String[] args) {
+		try {
+			new AdvancedRendering().start();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			Display.destroy();
+			System.exit(1);
+		}
 
-		// Genera un ID para la lista de visualizacion
-		displayList = glGenLists(1);
-
-		// Crea la lista de visualizacion usando el ID displayList
-		// Todas las llamadas posteriores para renderizado se almacenaran en la lista de visualizacion
-		glNewList(displayList, GL_COMPILE);
-
-		glBegin(GL_POINTS);
-		for (Point p : points)
-			glVertex3f(p.x, p.y, p.z);
-
-		glEnd();
-
-		// Deja de almacenar llamadas en la lista de visualizacion y compila
-		glEndList();
-
-		return displayList;
-
-	}
-
-	private void vao() {
-		/*
-		 * Crea un nuevo FloatBuffer (arreglo complejo de flotantes) con la capacidad de la longitud de los puntos * 3 (porque
-		 * tenemos 3 vertices por punto).
-		 */
-		vertexArray = BufferUtils.createFloatBuffer(points.length * 3);
-		// Itere los puntos y los guarda en el FloatBuffer
-		for (Point p : points)
-			vertexArray.put(new float[] { p.x, p.y, p.z });
-
-		// Hace que el buffer sea legible para OpenGL
-		vertexArray.flip();
-	}
-
-	private int vbo() {
-		// Crear el identificador para el VBO
-		vertexBufferObject = glGenBuffers();
-		// Vincular el VBO para su uso (en este caso: almacenar informacion)
-		glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
-		// Almacena todo el contenido del FloatBuffer en el VBO
-		glBufferData(GL_ARRAY_BUFFER, vertexArray, GL_STATIC_DRAW);
-		// Desatar la VBO
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-		return vertexBufferObject;
 	}
 
 	private static class Point {
@@ -293,15 +297,4 @@ public class AdvancedRendering {
 			this.z = z;
 		}
 	}
-
-	public static void main(String[] args) {
-		try {
-			new AdvancedRendering().start();
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-			Display.destroy();
-			System.exit(1);
-		}
-	}
-
 }
